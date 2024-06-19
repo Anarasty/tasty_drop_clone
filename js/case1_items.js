67299b93b74921a2878df6ed3ea61bfc8d4b3fbf
Anarasty <murloc.ninja16@gmail.com>
2024-06-19 13:37:06 +0300

    multi case script add

---
+++ js/case1_items.js
@@ -0,0 +1,13 @@
+const items = [
+    {name: "Crimson Gates of Nothl", img: 'imgs/tresure1_item1.png', chance: 5, price: 310},
+    {name: "Crimson Progenitor's Bane", img: 'imgs/tresure1_item2.png', chance: 10, price: 421},
+    {name: "Crimson First of the Flood", img: 'imgs/tresure1_item3.png', chance: 15, price: 593},
+    {name: "Crimson Latticean Hierarchy", img: 'imgs/tresure1_item4.png', chance: 20, price: 288},
+    {name: "Crimson Flight of Epiphany", img: 'imgs/tresure1_item5.png', chance: 25, price: 245},
+    {name: "Scavenging Guttleslug", img: 'imgs/tresure1_item6.png', chance: 30, price: 65},
+    {name: "Pale Mausoleum", img: 'imgs/tresure1_item7.png', chance: 35, price: 19},
+    {name: "Golden Ripper's Reel", img: 'imgs/tresure1_item8.png', chance: 40, price: 49},
+    {name: "Golden Edict of Shadows", img: 'imgs/tresure1_item9.png', chance: 45, price: 33},
+    {name: "Shadow in the Deep Dagger", img: 'imgs/tresure1_item10.png', chance: 50, price: 27},
+    {name: "Swift Claw", img: 'imgs/tresure1_item11.png', chance: 60, price: 84},
+];
