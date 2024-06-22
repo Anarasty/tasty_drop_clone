2900e4beeb1e04fe504330cff8228969e525c57b
Anarasty <murloc.ninja16@gmail.com>
2024-06-22 18:34:39 +0300

    third treasure

---
+++ js/case2_items.js
@@ -0,0 +1,17 @@
+const items = [
+    {name: "Flockheart's Gamble", img: 'imgs/tresure2_item1.png', chance: 5, price: 89},
+    {name: "Bladeform Legacy", img: 'imgs/tresure2_item2.png', chance: 10, price: 87},
+    {name: "Feast of Abscession", img: 'imgs/tresure2_item3.png', chance: 15, price: 88},
+    {name: "The Magus Cypher", img: 'imgs/tresure2_item4.png', chance: 20, price: 85},
+    {name: "Desert Burn Crossbow", img: 'imgs/tresure2_item5.png', chance: 25, price: 31},
+    {name: "Doomling", img: 'imgs/tresure2_item6.png', chance: 30, price: 18},
+    {name: "Ripper's Reel", img: 'imgs/tresure2_item7.png', chance: 35, price: 23},
+    {name: "Latticean Hierarchy", img: 'imgs/tresure2_item8.png', chance: 40, price: 20},
+    {name: "Vigil Signet", img: 'imgs/tresure2_item9.png', chance: 45, price: 33},
+    {name: "Onibi", img: 'imgs/tresure2_item10.png', chance: 50, price: 9},
+    {name: "Peregrine Flight", img: 'imgs/tresure2_item11.png', chance: 55, price: 19},
+    {name: "Jade Reckoning", img: 'imgs/tresure2_item12.png', chance: 60, price: 20},
+    {name: "Bracers of the Cavern Luminar", img: 'imgs/tresure2_item13.png', chance: 65, price: 11},
+    {name: "Golden Edict of Shadows", img: 'imgs/tresure2_item14.png', chance: 70, price: 15},
+    {name: "Rampant Outrage", img: 'imgs/tresure2_item15.png', chance: 75, price: 17},
+];
---
+++ js/case3_items.js
@@ -0,0 +1,3 @@
+const items = [
+    {name: "Flockheart's Gamble", img: 'imgs/tresure2_item1.png', chance: 5, price: 89},
+];
