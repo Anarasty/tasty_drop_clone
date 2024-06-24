117d07ea80abe4d17a5f45f9cf6ee6786a935651
Anarasty <murloc.ninja16@gmail.com>
2024-06-24 14:16:57 +0300

    third treasure done

--- js/case3_items.js
+++ js/case3_items.js
@@ -1,3 +1,17 @@
 const items = [
-    {name: "Flockheart's Gamble", img: 'imgs/tresure2_item1.png', chance: 5, price: 89},
+    {name: "Fire Lotus Belt", img: 'imgs/tresure3_item1.png', chance: 5, price: 429},
+    {name: "Dragonclaw Hook", img: 'imgs/tresure3_item2.png', chance: 10, price: 384},
+    {name: "Rainmaker", img: 'imgs/tresure3_item3.png', chance: 15, price: 355},
+    {name: "Pipe of Dezun", img: 'imgs/tresure3_item4.png', chance: 20, price: 321},
+    {name: "Kantusa the Script Sword", img: 'imgs/tresure3_item5.png', chance: 25, price: 295},
+    {name: "Shattered Greatsword", img: 'imgs/tresure3_item6.png', chance: 30, price: 270},
+    {name: "Drodo the Druffin", img: 'imgs/tresure3_item7.png', chance: 35, price: 242},
+    {name: "Lockjaw the Boxhound", img: 'imgs/tresure3_item8.png', chance: 40, price: 186},
+    {name: "Murrissey the Smeevil", img: 'imgs/tresure3_item9.png', chance: 45, price: 226},
+    {name: "Guard of the Red Mist", img: 'imgs/tresure3_item10.png', chance: 50, price: 173},
+    {name: "Tsukumo the Moon Cloud", img: 'imgs/tresure3_item11.png', chance: 55, price: 139},
+    {name: "Golden Severing Crest", img: 'imgs/tresure3_item12.png', chance: 60, price: 122},
+    {name: "Golden Shards of Exile", img: 'imgs/tresure3_item13.png', chance: 65, price: 110},
+    {name: "Golden Grasping Bludgeon", img: 'imgs/tresure3_item14.png', chance: 70, price: 148},
+    {name: "Golden Staff of Perplex", img: 'imgs/tresure3_item15.png', chance: 75, price: 162},
 ];
